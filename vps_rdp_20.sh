b7418719d451dd6c9f41612177292965e71c9943
Alis-Dev-idn <75902193+Alis-Dev-idn@users.noreply.github.com>
2021-07-07 05:26:22 +0700

    Add files via upload

---
+++ vps_rdp_20.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo apt-get -y install xfce4
+sudo apt-get -y install xrdp
+sudo systemctl enable xrdp
+echo xfce4-session >~/.xsession
+sudo service xrdp restart
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get -y install -f
+nano /opt/google/chrome/google-chrome
